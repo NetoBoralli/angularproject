505637227e5fb354a060a4967ea9aa14f007591e
Nelson <netoboralli@gmail.com>
2017-12-06 11:41:51 -0200

    Answer layout changed

--- src/app/rooms/question/question.component.ts
+++ src/app/rooms/question/question.component.ts
@@ -11,14 +11,19 @@ import { RoomComponent } from '../room/room.component';
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.css']
 })
-export class QuestionComponent implements OnInit {
+export class QuestionComponent implements OnInit, OnDestroy {
 
   inscription: Subscription
-  form: FormGroup
+  form: FormGroup;
+  answerForm: FormGroup
   key: string;
   qkey: string;
   question: any = {};
   new: boolean = false;
+  answers;
+  tags: any[] = [];
+  owner: boolean = false;
+  user = JSON.parse(localStorage.getItem('currentUser'));
 
   constructor(
     private roomsService: RoomsService,
@@ -27,31 +32,82 @@ export class QuestionComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.owner = this.user.username == this.roomComponent.room.owner ? true : false;
+
     this.inscription = this.aroute.params.subscribe((params: any) => {
       this.key = this.roomComponent.key;
       this.qkey = params['qkey'];
 
       this.roomsService.getQuestionByKey(this.key, this.qkey).subscribe((data) => {
-        if (data){
+        if (data) {
           this.question = data;
 
-        }else{
+        } else {
           this.new = true;
         }
-
+        this.roomsService.getAnswers(this.key, this.qkey).subscribe((data) => {
+          this.answers = data.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+          this.sortAnswers();
+        });
       });
     })
 
     this.form = new FormGroup({
       question: new FormControl(null)
     });
+
+    this.answerForm = new FormGroup({
+      answer: new FormControl(null)
+    });
+  }
+
+  ngOnDestroy() {
+    this.roomComponent.sidenav.close();
+  }
+
+  sortAnswers() {
+    this.tags = [];
+    this.answers.forEach(answer => {
+      let pos = this.tags.findIndex(t => t.tag === answer.tag);
+      if (pos === -1) {
+        this.tags.push({ tag: answer.tag, answers: [] });
+        this.tags[this.tags.length - 1].answers.push(answer);
+      } else
+        this.tags[pos].answers.push(answer);
+    });
+    this.tags.sort((a, b) => {
+      if (a.tag > b.tag)
+        return 1;
+      return -1;
+    })
   }
 
   insertQuestion() {
+    if (this.form.get('question').value) {
+      let owner = JSON.parse(localStorage.getItem('currentUser'));
+      this.roomsService.setQuestion(this.key, this.form.get('question').value, owner.username).then(data => {
+        console.log(data);
+      })
+      this.form.reset();
+    }
+  }
+
+  setTF(answer) {
+    answer.isEditing = !answer.isEditing;
+  }
+
+  updateTag(answer) {
+    this.roomsService.setTag(this.key, this.qkey, answer.key, answer.tag).then(() => {
+      this.sortAnswers();
+      this.setTF(answer);
+    });
+  }
+
+  addAnswer() {
     let owner = JSON.parse(localStorage.getItem('currentUser'));
-    this.roomsService.setQuestion(this.key, this.form.get('question').value, owner.username).then(data => {
-      console.log(data);
-    })
-    this.form.reset();
+    this.roomsService.setAnswer(this.key, this.qkey, this.answerForm.get('answer').value, this.user.username)
+      .then(res => {
+        this.answerForm.reset();
+      });
   }
 }
